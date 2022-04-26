cb018d40807bddf79ecc923ec468921fcfa2ee7d
한송희 <98_han@naver.com>
2022-04-26 16:59:16 +0900

    Create .csv file to save data

--- Tkinter/tutorial/main.py
+++ Tkinter/tutorial/main.py
@@ -2,8 +2,48 @@
 
 from tkinter import *
 from tkinter import messagebox
+import pandas
 
 BACKGROUND_COLOR = "#223441"
+
+# ---------------------------- READ FILE ----------------------------- #
+
+data = pandas.read_csv("todo_list.csv", index_col=0)
+print(data)
+todolist = data["list"].to_list()
+
+
+# ---------------------------- TASK FUNCTION ----------------------------- #
+
+def new_task():
+    task = my_entry.get()
+    if task != "":
+        listbox.insert(END, task)
+        todolist.append(task)
+        my_entry.delete(0, END)
+
+    else:
+        messagebox.showwarning("warning", "Please write some task.")
+
+
+def delete_task():
+    for i in listbox.curselection():
+        list_item = str(listbox.get(i))
+    listbox.delete(ANCHOR)
+    todolist.remove(list_item)
+
+
+def on_save():
+    new_data = pandas.DataFrame(todolist, columns=["list"])
+    new_data.to_csv("todo_list.csv")
+
+
+def on_exit():
+    if messagebox.askokcancel(title="Warning", message="Do you want to quit? \n Unsaved files will be deleted."):
+        window.destroy()
+
+
+# ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.geometry("500x450+500+200")
@@ -13,24 +53,19 @@ window.title("To Do List✏️")
 frame = Frame(window)
 frame.pack(pady=10)
 
+menubar = Menu(window)
+file_menu = Menu(menubar, tearoff=0)
+
+file_menu.add_command(label="저장", command=on_save)
+file_menu.add_command(label="끝내기", command=on_exit)
+
+menubar.add_cascade(label="파일", menu=file_menu)
+
 listbox = Listbox(frame, width=25, height=8, font=("Arial", 18), bd=0, fg="white", bg=BACKGROUND_COLOR,
                   activestyle="none")
 listbox.pack(side=LEFT, fill=BOTH)
 
-task_list = [
-    'Eat apple',
-    'drink water',
-    'go gym',
-    'write software',
-    'write documentation',
-    'take a nap',
-    'Learn something',
-    'paint canvas',
-    'something to drink',
-    'play with pingping'
-]
-
-for item in task_list:
+for item in todolist:
     listbox.insert(END, item)
 
 scrollbar = Scrollbar(frame, orient=VERTICAL)
@@ -46,25 +81,11 @@ my_entry.pack(pady=20)
 button_frame = Frame(window)
 button_frame.pack(pady=20)
 
-
-def new_task():
-    task = my_entry.get()
-    if task != "":
-        listbox.insert(END, task)
-        my_entry.delete(0, END)
-    else:
-        messagebox.showwarning("warning", "Please write some task.")
-
-
 add_button = Button(button_frame, text="ADD", font=("Arial", 20), padx=20, pady=10, command=new_task)
 add_button.pack(fill=BOTH, expand=True, side=LEFT)
-
-
-def delete_task():
-    listbox.delete(ANCHOR)
-
 
 delete_button = Button(button_frame, text="DELETE", font=("Arial", 20), padx=20, pady=10, command=delete_task)
 delete_button.pack(fill=BOTH, expand=True, side=RIGHT)
 
+window.config(menu=menubar)
 window.mainloop()
