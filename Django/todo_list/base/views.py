ae7f083a07e734a535e355ddc3a4708e0709cb4b
한송희 <98_han@naver.com>
2022-05-09 17:28:28 +0900

    Styling our pages

--- Django/todo_list/base/views.py
+++ Django/todo_list/base/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView,UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 from .models import Task
+
+from django.views import View
+from django.shortcuts import redirect
+from django.db import transaction
+
+from .models import Task
+from .forms import PositionForm
+
 
 class TaskList(ListView):
     model = Task
@@ -14,7 +22,7 @@ class TaskList(ListView):
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
             context['tasks'] = context['tasks'].filter(title__icontains=search_input)
-
+            context['counts'] = context['tasks'].filter(complete=False).count()
         context['search_input'] = search_input
 
         return context
@@ -38,3 +46,15 @@ class DeleteView(DeleteView):
     model = Task
     context_object_name =  'task'
     success_url = reverse_lazy('tasks')
+
+class TaskReorder(View):
+    def post(self, request):
+        form = PositionForm(request.POST)
+
+        if form.is_valid():
+            positionList = form.cleaned_data["position"].split(',')
+
+            with transaction.atomic():
+                self.request.user.set_task_order(positionList)
+
+        return redirect(reverse_lazy('tasks'))
